40dd7c5c2e72f911f09ace7ac686a81533c56f04
Alexander Kravchuk <a.krauchuk@godeltech.com>
2017-08-08 15:40:49 +0300

    Create CategoryController with a child action returning a list of categories. Create a partial view for this list. Render the partial view as a part of Home/Index view.

---
+++ Training/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Training.Infrastructure.Interfaces;
+
+namespace Training.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Categories() => PartialView("_CategoriesPartial", categoryRepository.Categories);
+    }
+}
