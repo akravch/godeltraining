2072ca791903c55fb7e59af0778325f0078f1822
Alexander Kravchuk <a.krauchuk@godeltech.com>
2017-08-08 15:22:45 +0300

    Add PostRepository.

---
+++ Training.Insrastructure/PostRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using Training.Domain;
+using Training.Dto;
+
+namespace Training.Insrastructure
+{
+    public class PostRepository
+    {
+        private readonly DataContext context;
+
+        public PostRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<PostDto> Posts => context.Posts.Select(post => ToDto(post));
+
+        public void Add(PostDto postDto)
+        {
+            var categoryId = postDto.Category.Id;
+            var postCategory = context.Categories.FirstOrDefault(category => category.Id == categoryId);
+
+            if (postCategory != null)
+            {
+                context.Posts.Add(new Post
+                {
+                    Body = postDto.Body,
+                    CreationDateTimeUtc = postDto.CreationDateTimeUtc,
+                    Category = postCategory
+                });
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(PostDto postDto)
+        {
+            var existingPost = postDto.Id != default(Guid) ? context.Posts.FirstOrDefault(post => post.Id == postDto.Id) : null;
+
+            if (existingPost != null)
+            {
+                existingPost.Body = postDto.Body;
+                existingPost.CreationDateTimeUtc = postDto.CreationDateTimeUtc;
+
+                if (existingPost.Category.Id != postDto.Category.Id)
+                {
+                    var newCategory = context.Categories.FirstOrDefault(category => category.Id == postDto.Category.Id);
+
+                    if (newCategory != null)
+                    {
+                        existingPost.Category = newCategory;
+                    }
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        public void Remove(PostDto postDto)
+        {
+            var postToRemove = postDto.Id != default(Guid) ? context.Posts.FirstOrDefault(post => post.Id == postDto.Id) : null;
+
+            if (postToRemove != null)
+            {
+                context.Posts.Remove(postToRemove);
+                context.SaveChanges();
+            }
+        }
+
+        private static PostDto ToDto(Post post)
+        {
+            return new PostDto
+            {
+                Id = post.Id,
+                Body = post.Body,
+                CreationDateTimeUtc = post.CreationDateTimeUtc,
+                Category = new CategoryDto
+                {
+                    Id = post.Category.Id,
+                    Name = post.Category.Name
+                }
+            };
+        }
+    }
+}
