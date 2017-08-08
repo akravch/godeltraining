fb7e529f904cd4ac6e121d4909435f67611f9138
Alexander Kravchuk <a.krauchuk@godeltech.com>
2017-08-08 12:30:06 +0300

    Add a Post class to Training.Domain.

---
+++ Training.Domain/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Training.Domain
+{
+    public class Post
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        
+        public string Body { get; set; }
+    }
+}
