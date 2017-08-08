850008ec77add0f267f52981f600918882444604
Alexander Kravchuk <a.krauchuk@godeltech.com>
2017-08-08 14:52:30 +0300

    Add Infrastructure and Infrastructure.Interfaces projects with simple CategoryRepository.

---
+++ Training.Infrastructure.Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Training.Domain;
+
+namespace Training.Infrastructure.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IQueryable<CategoryDto> Categories { get; }
+        
+        void Add(CategoryDto categoryDto);
+
+        void Update(CategoryDto categoryDto);
+
+        void Remove(CategoryDto categoryDto);
+    }
+}
---
+++ Training.Insrastructure/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Training.Domain;
+using Training.Infrastructure.Interfaces;
+
+namespace Training.Insrastructure
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DataContext context;
+
+        public CategoryRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<CategoryDto> Categories => context.Categories.Select(category => new CategoryDto { Id = category.Id, Name = category.Name });
+
+        public void Add(CategoryDto categoryDto)
+        {
+            context.Categories.Add(new Category { Name = categoryDto.Name });
+            context.SaveChanges();
+        }
+
+        public void Update(CategoryDto categoryDto)
+        {
+            var existingCategory = categoryDto.Id != default(Guid) ? context.Categories.FirstOrDefault(category => category.Id == categoryDto.Id) : null;
+
+            if (existingCategory != null)
+            {
+                existingCategory.Name = categoryDto.Name;
+                context.SaveChanges();
+            }
+        }
+
+        public void Remove(CategoryDto categoryDto)
+        {
+            var categoryToRemove = categoryDto.Id != default(Guid) ? context.Categories.FirstOrDefault(category => category.Id == categoryDto.Id) : null;
+
+            if (categoryToRemove != null)
+            {
+                context.Categories.Remove(categoryToRemove);
+                context.SaveChanges();
+            }
+        }
+    }
+}
