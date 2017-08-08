0c4f1c45821629caca653fb55f3f676c2365bbdc
Alexander Kravchuk <a.krauchuk@godeltech.com>
2017-08-08 13:28:30 +0300

    Enable migrations.

---
+++ Training.Domain/Migrations/201708081000370_InitialCreate.cs
@@ -0,0 +1,41 @@
+namespace Training.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false, identity: true),
+                        Body = c.String(),
+                        Category_Id = c.Guid(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.Category_Id)
+                .Index(t => t.Category_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Posts", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Posts", new[] { "Category_Id" });
+            DropTable("dbo.Posts");
+            DropTable("dbo.Categories");
+        }
+    }
+}
