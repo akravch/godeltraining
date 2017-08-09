c4f10edfbf66f8c1cf8570e60f40f9df81b0319f
Alexander Kravchuk <a.krauchuk@godeltech.com>
2017-08-09 11:11:18 +0300

    Add HomeController.Post action and the view for it.

--- Training/Controllers/HomeController.cs
+++ Training/Controllers/HomeController.cs
@@ -1,12 +1,42 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Training.Infrastructure.Interfaces;
 
 namespace Training.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPostRepository postRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public HomeController(IPostRepository postRepository, ICategoryRepository categoryRepository)
+        {
+            this.postRepository = postRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Posts(string category = null)
+        {
+            ActionResult result;
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                var foundCategory = categoryRepository.Categories.FirstOrDefault(existingCategory => existingCategory.Name.Equals(category, StringComparison.OrdinalIgnoreCase));
+
+                result = foundCategory != null ? View(foundCategory.Posts) : (ActionResult)HttpNotFound();
+            }
+            else
+            {
+                result = View(postRepository.Posts.ToList());
+            }
+
+            return result;
         }
     }
 }
