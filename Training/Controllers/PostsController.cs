bcf0779ea1d2d6f2bcb3dab1de34decdfea5fa63
Alexander Kravchuk <a.krauchuk@godeltech.com>
2017-08-10 15:08:53 +0300

    Add EditPostViewModelExtensions. Extract some common stuff to methods in PostController.

--- Training/Controllers/PostsController.cs
+++ Training/Controllers/PostsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Mvc;
 using Training.Domain;
 using Training.Dto;
+using Training.Extensions;
 using Training.Infrastructure.Interfaces;
 using Training.Models;
 using Training.ViewModels;
@@ -50,31 +51,12 @@ namespace Training.Controllers
         public ActionResult New(EditPostViewModel postViewModelViewModel)
         {
             ActionResult result;
+            var changedPost = ChangePost(postViewModelViewModel, post => postService.AddNewPost(post));
 
-            var isModelValid = ModelState.IsValid;
-
-            if (isModelValid)
+            if (changedPost != null)
             {
-                var selectedCategory = categoryService.GetCategoryById(postViewModelViewModel.SelectedCategoryId);
-
-                if (selectedCategory != null)
-                {
-                    postService.AddNewPost(new Post
-                    {
-                        Title = postViewModelViewModel.Title,
-                        Body = postViewModelViewModel.Body,
-                        Category = selectedCategory,
-                        CreationDateTimeUtc = DateTime.UtcNow
-                    });
-                    TempData[TempDataConstants.PostChanged] = "The new post has been succesfully published";
-                    result = RedirectToAction("Index", new { category = selectedCategory.Name });
-                }
-                else
-                {
-                    ModelState.AddModelError(nameof(postViewModelViewModel.SelectedCategoryId), "Selected category does not exist");
-                    postViewModelViewModel.Categories = GetCategories();
-                    result = View(postViewModelViewModel);
-                }
+                TempData[TempDataConstants.PostChanged] = "The post has been succesfully published";
+                result = RedirectToAction("Index", new { category = changedPost.Category.Name });
             }
             else
             {
@@ -87,24 +69,19 @@ namespace Training.Controllers
 
         public ActionResult Edit(string id)
         {
-            Guid guid;
+            var post = GetPostByStringId(id);
 
-            if (Guid.TryParse(id, out guid))
+            if (post != null)
             {
-                var post = postService.GetPostById(guid);
-
-                if (post != null)
+                var postViewModel = new EditPostViewModel
                 {
-                    var postViewModel = new EditPostViewModel
-                    {
-                        Title = post.Title,
-                        Body = post.Body,
-                        Categories = GetCategories(),
-                        SelectedCategoryId = post.Category.Id
-                    };
+                    Title = post.Title,
+                    Body = post.Body,
+                    Categories = GetCategories(),
+                    SelectedCategoryId = post.Category.Id
+                };
 
-                    return View(postViewModel);
-                }
+                return View(postViewModel);
             }
 
             return HttpNotFound();
@@ -114,31 +91,12 @@ namespace Training.Controllers
         public ActionResult Edit(EditPostViewModel postViewModelViewModel)
         {
             ActionResult result;
+            var changedPost = ChangePost(postViewModelViewModel, post => postService.UpdatePost(post));
 
-            if (ModelState.IsValid)
+            if (changedPost != null)
             {
-                var selectedCategory = categoryService.GetCategoryById(postViewModelViewModel.SelectedCategoryId);
-
-                if (selectedCategory != null)
-                {
-                    var post = new Post
-                    {
-                        Id = postViewModelViewModel.Id,
-                        Title = postViewModelViewModel.Title,
-                        Body = postViewModelViewModel.Body,
-                        Category = selectedCategory
-                    };
-
-                    postService.UpdatePost(post);
-                    TempData[TempDataConstants.PostChanged] = "The post has been succesfully saved";
-                    result = RedirectToAction("Index", new { category = selectedCategory.Name });
-                }
-                else
-                {
-                    ModelState.AddModelError(nameof(EditPostViewModel.SelectedCategoryId), "Selected category does not exist");
-                    postViewModelViewModel.Categories = GetCategories();
-                    result = View(postViewModelViewModel);
-                }
+                TempData[TempDataConstants.PostChanged] = "The post has been succesfully saved";
+                result = RedirectToAction("Index", new { category = changedPost.Category.Name });
             }
             else
             {
@@ -152,19 +110,14 @@ namespace Training.Controllers
         [HttpPost]
         public ActionResult Delete(string id)
         {
-            Guid guid;
+            var post = GetPostByStringId(id);
 
-            if (Guid.TryParse(id, out guid))
+            if (post != null)
             {
-                var post = postService.GetPostById(guid);
+                postService.DeletePost(post);
+                TempData[TempDataConstants.PostChanged] = "The post has been succesfully deleted";
 
-                if (post != null)
-                {
-                    postService.DeletePost(post);
-                    TempData[TempDataConstants.PostChanged] = "The post has been succesfully deleted";
-
-                    return Redirect(Request.UrlReferrer.AbsolutePath);
-                }
+                return Redirect(Request.UrlReferrer.AbsolutePath);
             }
 
             return HttpNotFound();
@@ -180,6 +133,40 @@ namespace Training.Controllers
                 Id = category.Id,
                 Name = category.Name
             }).ToList();
+        }
+
+        private Post GetPostByStringId(string id)
+        {
+            Guid guid;
+
+            return Guid.TryParse(id, out guid) ? postService.GetPostById(guid) : null;
+        }
+
+        private void AddMissingCategoryError(string propertyName)
+        {
+            ModelState.AddModelError(propertyName, "Selected category does not exist");
+        }
+
+        private Post ChangePost(EditPostViewModel viewModel, Action<Post> changeAction)
+        {
+            Post changedPost = null;
+
+            if (ModelState.IsValid)
+            {
+                var selectedCategory = categoryService.GetCategoryById(viewModel.SelectedCategoryId);
+
+                if (selectedCategory != null)
+                {
+                    changedPost = viewModel.ToPost(selectedCategory);
+                    changeAction(changedPost);
+                }
+                else
+                {
+                    AddMissingCategoryError(nameof(viewModel.SelectedCategoryId));
+                }
+            }
+
+            return changedPost;
         }
     }
 }
---
+++ Training/Extensions/EditPostViewModelExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Training.Domain;
+using Training.ViewModels;
+
+namespace Training.Extensions
+{
+    public static class EditPostViewModelExtensions
+    {
+        public static Post ToPost(this EditPostViewModel viewModel, Category category)
+        {
+            return new Post
+            {
+                Id = viewModel.Id,
+                Title = viewModel.Title,
+                Body = viewModel.Body,
+                Category = category
+            };
+        }
+    }
+}
